dacf24f5e06e150beb2e6eb78722b363ccf489d7
Zimovik007 <zimovik@list.ru>
2017-04-02 13:06:01 +1000

    improve test.sh

--- test.sh
+++ test.sh
@@ -7,14 +7,7 @@ for filename in $script_dir/tests/*.in; do
 	$script_dir/a.out $filename >$output
 	diff $output $answer && rm -f $output
 done
-for filename in $script_dir/tests/*.errin; do
-	echo ..................................................$filename
-	output=$script_dir/tests/$(basename "$filename" .errin).out
-	answer=$script_dir/tests/$(basename "$filename" .errin).std
-	$script_dir/a.out $filename 2>$output >/dev/null
-	diff $output $answer && rm -f $output
-done
 total=$(find $script_dir/tests/*.std 2>/dev/null | wc -l)
 failed=$(find $script_dir/tests/*.out 2>/dev/null | wc -l)
 passed=`expr $total - $failed`
-echo $passed of $total tests passed
+echo $passed of $total tests passedы
