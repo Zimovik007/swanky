28aa2cc465dfc964dccd5847312ba07564e104fc
Zimovik007 <zimovik@list.ru>
2017-04-06 12:35:37 +1000

    add key '--lex', improve test.sh

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+	string lex = "--lex";
 	Scanner lexical_analyzer(argv[1]);
-	if (!lexical_analyzer.GetCntErrors())
-		for (int i = 0; i < lexical_analyzer.GetLengthDeque() - 1; i++){
-			lexical_analyzer.GetToken(i).PrintToken();
-		}
+	if (argv[2] == lex){
+		if (!lexical_analyzer.GetCntErrors())
+			for (int i = 0; i < lexical_analyzer.GetLengthDeque() - 1; i++){
+				lexical_analyzer.GetToken(i).PrintToken();
+			}
+	}
 	return 0;
 }
